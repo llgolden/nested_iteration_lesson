d83b240b8fc2c895ce2256be0459e4c5e4201ec6
Kat Brandt <96136707+KatBrandt@users.noreply.github.com>
2023-04-06 12:39:25 -0600

    Add additional code to nested iteration warmup

--- warmup/warmup.rb
+++ warmup/warmup.rb
@@ -1,3 +1,20 @@
+# Part One
+# Given the follow array:
+animals = [:dog, :cat, :zebra, :quokka, :unicorn, :bear]
+
+# Use an enumerable to
+# 1. Return an array of animals as strings.
+
+
+
+
+# 2. Return an array of animals with a length >= 4.
+
+
+
+
+
+# Part Two
 # Given the following array:
 nested_animals = [[:dog, :cat, :zebra], [:quokka, :unicorn, :bear]]
 
